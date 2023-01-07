b406d2713ed177c387d32aae4804a14d871d3968
Gold4Task <122135139+Gold4Task@users.noreply.github.com>
2023-01-07 02:55:37 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1673056385,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/back-sheet0.png",
+		"images/logo-sheet0.png",
+		"images/9patch.png",
+		"images/tl_youtube-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tl_youtube2-sheet0.png",
+		"images/tl_youtube3-sheet0.png",
+		"images/pic1-sheet0.png",
+		"images/pic2-sheet0.png",
+		"images/pic3-sheet0.png",
+		"media/sound1.m4a",
+		"media/sound1.ogg",
+		"media/sound2.m4a",
+		"media/sound2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
